c59f46ce439baac9fa2ac586913a1d4c545cfb17
nathanluu <nate1299@me.com>
2020-09-29 00:41:26 -0700

    Initial Commit

---
+++ Beast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beast
+//
+//  Created by Nathan Luu on 9/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
