17e69f3cb285df96cafd0bbd55e91675f0dfb2dc
nathanluu <nate1299@me.com>
2020-09-29 11:15:29 -0700

    Refactored out  navigation controllers

--- Beast/ViewController.swift
+++ Beast/ViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonRef: UIButton!
+    @IBOutlet weak var labelRef: UILabel!
+    @IBOutlet weak var textBox: UITextField!
+    
+    @IBAction func changeLabelText(_ sender: Any) {
+        labelRef.text = textBox.text
+    }
+    
+    @IBAction func onTextChange(_ sender: Any) {
+        labelRef.text = textBox.text
+    }
+    @IBAction func onBlue(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
